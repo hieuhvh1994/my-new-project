14e1fa8a943fdab3313331596a5bb4a27b04b81f
hieuhvh1994 <hieudaohn94@gmail.com>
2021-08-17 16:24:24 +0700

    Add folder UngDungMayTinh

--- UngDungMayTinh/UngDungMayTinh.js
+++ UngDungMayTinh/UngDungMayTinh.js
@@ -1,14 +1,38 @@
+let num1=0
+let com=""
+
+
 function btn(val)
 {
     document.getElementById("result").value+=val
 }
 
 
+
+function cal(val){
+    num1 = parseFloat(document.getElementById("result").value)
+    document.getElementById("result").value = ""
+    com=val
+}
+
 function solve()
 {
-    let x = document.getElementById("result").value
-    let y = eval(x)
-    document.getElementById("result").value = y
+    let num2 = parseFloat(document.getElementById("result").value)
+    if (com==="+") {
+        let result = num1+num2
+        document.getElementById("result").value = result.toFixed(2)
+    } else if (com==="-") {
+        let result = num1 - num2
+        document.getElementById("result").value = result.toFixed(2)
+    } else if (com==="*") {
+        let result = num1*num2
+        document.getElementById("result").value = result.toFixed(2)
+    } else if (com==="/"){
+        let result = num1/num2
+        document.getElementById("result").value = result.toFixed(2)
+    }
+
+
 }
 
 
